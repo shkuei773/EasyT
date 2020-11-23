870bfa5c0e9ed720a5323754c4c5e5149d4ec3c9
PNG_ <45896529+shkuei773@users.noreply.github.com>
2020-11-23 16:37:37 +0900

    Update LoadingBar.py

--- Python/Image to Pdf/LoadingBar.py
+++ Python/Image to Pdf/LoadingBar.py
@@ -3,4 +3,14 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 class LoadingState(QDialog):
-
+  def __init__(self):
+    super().__init__()
+    self.LoadingLayout()
+    
+  def LoadingLayout(self):
+    self.setGeometry(1100, 200, 300, 100)
+    self.setWindowTitle("Loading Bar")
+    layout = QGridLayout()
+    layout.addWidget(QLabel("In Progress"),0,0)
+    self.setLayout(layout)
+    
