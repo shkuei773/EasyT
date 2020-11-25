71bc1063efba1b31ac4fe5393be0cccad82e6b44
PNG_ <45896529+shkuei773@users.noreply.github.com>
2020-11-25 10:55:43 +0900

    Update DBClass.cs

--- C#/ComputerScheduling/ComputerScheduling/DB/DBClass.cs
+++ C#/ComputerScheduling/ComputerScheduling/DB/DBClass.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.DataSqlClient;
 
 namespace ComputerScheduling.DB
 {
     class DBClass
-    {
+    {      
+        private SqlConnection con = null;
+        public DBClass()
+        public String server{get;set;}
+        public String dbName{get;set;}
+        public String userId{get;set;}
+        public String passWord{get;set;}
+        
+        public DBConnection()
+        {
+            con = new SqlConnection($"Data Source={server}; Initial Catalog={dbName}; User ID={userId}; Password={passWord});
+        }        
     }
 }
