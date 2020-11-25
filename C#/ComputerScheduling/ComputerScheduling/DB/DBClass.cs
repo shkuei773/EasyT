b1b0b8c54f8cf297cf963e5eac1aadac5c7cf33a
PNG_ <45896529+shkuei773@users.noreply.github.com>
2020-11-25 11:14:05 +0900

    Update DBClass.cs

--- C#/ComputerScheduling/ComputerScheduling/DB/DBClass.cs
+++ C#/ComputerScheduling/ComputerScheduling/DB/DBClass.cs
@@ -6,20 +6,57 @@ using System.Threading.Tasks;
 using System.Data;
 using System.DataSqlClient;
 
+define 
+
 namespace ComputerScheduling.DB
 {
-    class DBClass
+    public class DBClass
     {      
-        private SqlConnection con = null;
+        private static SqlConnection con = null; // static => 객체를 생성해서 사용하는 것이 아니라 클래스를 직접사용..?하는건가..
+        private static bool SqlConState = false; 
         public DBClass(){}
         public String server{get;set;}
         public String dbName{get;set;}
         public String userId{get;set;}
         public String passWord{get;set;}
         
-        public DBConnection()
+        #region DBConnec
+        public static DBConnect()
         {
-            con = new SqlConnection($"Data Source={server}; Initial Catalog={dbName}; User ID={userId}; Password={passWord}");
-        }        
+            try
+            {
+                if(!SqlConState)
+                {
+                    con = new SqlConnection($"Data Source={server}; Initial Catalog={dbName}; User ID={userId}; Password={passWord}");
+                }                                 
+                
+            }
+            catch(Exception e)
+            {
+                
+            }
+            finally
+            {
+            
+            }
+            
+        }   
+        #endregion
+        
+        #region DBDisConnect
+        public static DBDisConnect()
+        {
+            try
+            {
+            }
+            catch(Exception e)
+            {
+            }
+            finally
+            {
+            }
+        }
+        #endregion
+        
     }
 }
