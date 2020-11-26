13089f8bee45eb0e102d14b4773572d29c593a4e
PNG_ <45896529+shkuei773@users.noreply.github.com>
2020-11-26 10:40:46 +0900

    Update Form1.cs

--- C#/ComputerScheduling/ComputerScheduling/Form1.cs
+++ C#/ComputerScheduling/ComputerScheduling/Form1.cs
@@ -12,9 +12,40 @@ namespace ComputerScheduling
 {
     public partial class Form1 : Form
     {
+        Timer timer = null;
         public Form1()
         {
             InitializeComponent();
         }
+        //////////////////////////////////////  test
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer ??= new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // 1초
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+        
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop(); // => label1.Text = ""; timer = null;
+        }
+        
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            timer.Pause();
+        }
+        
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer.Reset(); // => label1.Text = ""; timer = null;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {            
+            label1.Text = DateTime.Now.ToLongTimeString();
+        } 
+        ////////////////////////////////////// test
+        
     }
 }
